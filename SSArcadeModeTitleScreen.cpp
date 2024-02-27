240fcb97f6fc9db4cae2cf637c1afac92ea09979
Ian Martell <tayoInteractive@gmail.com>
2024-02-27 09:55:45 -0500

    Update SSArcadeModeTitleScreen.cpp

--- SSArcadeModeTitleScreen.cpp
+++ SSArcadeModeTitleScreen.cpp
@@ -24,6 +24,16 @@ const FMargin CalculateSubTitlePosition(FVector2D viewportSize)
 	return FMargin(leftPad, topPad, rightPad, bottomPad);
 }
 
+const FMargin CalculateMenuTextPosition(FVector2D viewportSize, int textIndex)
+{
+	float leftPad = viewportSize.X * 0.2;
+	float topPad = viewportSize.Y * (0.3 + textIndex);
+	float rightPad = viewportSize.X * 0.6;
+	float bottomPad = viewportSize.Y * (0.625 - textIndex);
+
+	return FMargin(leftPad, topPad, rightPad, bottomPad);
+}
+
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SSArcadeModeTitleScreen::Construct(const FArguments& InArgs)
 {
